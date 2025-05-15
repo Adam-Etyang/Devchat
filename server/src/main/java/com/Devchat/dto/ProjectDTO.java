72c7edd263eacb6a307e2e7f12f56189a9fca25c
Adam-Etyang <adametyang69>
2025-05-15 19:15:35 +0300

    resolving imports and syntax errors

---
+++ server/src/main/java/com/Devchat/dto/ProjectDTO.java
@@ -0,0 +1,12 @@
+package com.Devchat.dto;
+
+import lombok.Data;
+
+@Data
+public class ProjectDTO {
+  private Long id;
+  private String name;
+  private String decription;
+  private Long creatorID;
+
+}
