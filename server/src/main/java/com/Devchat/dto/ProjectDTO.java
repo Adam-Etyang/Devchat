8b648fa23acd69c7270a67ad3f01942af622ee93
Adam-Etyang <adametyang69>
2025-05-18 00:03:54 +0300

    commit

--- server/src/main/java/com/Devchat/dto/ProjectDTO.java
+++ server/src/main/java/com/Devchat/dto/ProjectDTO.java
@@ -4,9 +4,9 @@ import lombok.Data;
 
 @Data
 public class ProjectDTO {
-  private Long id;
-  private String name;
-  private String decription;
-  private Long creatorID;
+  private Long Id;
+  private String Name;
+  private String Decription;
+  private Long CreatorID;
 
 }
