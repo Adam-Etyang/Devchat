19deab864c8d3730c2686b9987aba49ee04b1ac0
Adam-Etyang <adam.etyang0@gmail.com>
2025-05-28 11:17:21 +0300

    basic implementation and tests on api endpoints

---
+++ src/main/java/com/Devchat/config/securityconfig.java
@@ -0,0 +1,20 @@
+package com.Devchat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class securityconfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+        return http.build();
+    }
+}
